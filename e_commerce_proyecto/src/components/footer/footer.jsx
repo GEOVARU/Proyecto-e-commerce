1ed53e4792de1c1b213cec44ac916af16fdcbd20
GEOVARU <alfrediviris.1@gmail.com>
2023-11-02 23:24:51 -0600

    CAMBIOS FOOTER

--- e_commerce_proyecto/src/components/footer/footer.jsx
+++ e_commerce_proyecto/src/components/footer/footer.jsx
@@ -8,27 +8,23 @@ const footer = () => {
         <>
             <div class="footer-basic">
                 <footer>
-                    <ul class="list-inline">
-                        <li class="list-inline-item">
-                            <a href="#">
-                                <strong>9490-20-427</strong><br />
-                                Alfredo Geovanni Ramirez Tzunun<br />
-                                <a href="mailto:aramirezt7@miumg.edu.gt">aramirezt7@miumg.edu.gt</a>
-                            </a>
-                        </li>
+                    <div class="footer-content">
+                        <div class="footer-column">
+                            <strong>9490-20-427</strong><br />
+                            Alfredo Geovanni Ramirez Tzunun<br />
+                            <a href="mailto:aramirezt7@miumg.edu.gt">aramirezt7@miumg.edu.gt</a>
+                        </div>
 
-                        <li class="list-inline-item">
-                            <a href="#">
-                                <strong>9490-20-387</strong><br />
-                                Rudy Alexander Amado Soto Rosil<br />
-                                <a href="mailto:rsotor2@miumg.edu.gt">rsotor2@miumg.edu.gt</a>
-                            </a>
-                        </li>
-                    </ul>
-
-                    <p class="copyright">Desarrollo Web Sección "A"</p>
+                        <div class="footer-column">
+                            <strong>9490-20-387</strong><br />
+                            Rudy Alexander Amado Soto Rosil<br />
+                            <a href="mailto:rsotor2@miumg.edu.gt">rsotor2@miumg.edu.gt</a>
+                        </div>
+                    </div>
+                    <p class="copyright">Desarrollo Web, 8vo Semestre UMG</p>
                 </footer>
             </div>
+
 
         </>
     );
