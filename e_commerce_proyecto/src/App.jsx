1dd57e8e938236e6c3b1905f0639ca71a1158d65
GEOVARU <alfrediviris.1@gmail.com>
2023-11-02 22:47:12 -0600

    Primera Version proyecto
    Subiremos la prmera version del proyecto

---
+++ e_commerce_proyecto/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+/*NOTA CAMBIAR LOS NOMBRES Y RUTAS DE LOS COMPONENTES PARA QUE FUNCIONE*/
+import Menu from './components/MainMenu/MainMenu';
+import Prods from './components/productos/productos';
+import LogIn from './components/headerButtons/LogIn';
+import SignIn from './components/headerButtons/SignIn';
+import Footer from './components/footer/footer';
+import Buy from './components/BuyForm/BuyForm';
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleCategorySelect = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const handleError = () => {
+    setLoggedIn(false);
+  };
+
+  return (
+    <Router>
+      <Menu onLoginClick={() => setLoggedIn(false)} onSignInClick={() => setLoggedIn(false)} />
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+        <Route path="/productos" element={<Prods onError={handleError} />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/checkout" element={<Buy />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ e_commerce_proyecto/src/components/footer/footer.jsx
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import "./footer.css";
+import "./footer.sass";
+
+const footer = () => {
+  return (
+    <>
+    <div class="footer-basic">
+        <footer>
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a href="#">
+                    9490-20-427<br/>
+                   Alfredo Geovanni Ramirez Tzunun<br/>
+                   aramirezt7@miumg.edu.gt<br/>
+                    </a>
+                </li>
+
+                <li class="list-inline-item">
+                    <a href="#">
+                    9490-20-387<br/>
+                    Rudy sarazua xd<br/>
+                    rskiller2@miumg.edu.gt<br/>
+                    </a>
+                </li>
+            </ul>
+
+            <p class="copyright">Desarrollo Web Sección "A"</p>
+        </footer>
+    </div>
+
+    </>
+  );
+}
+
+export default footer
